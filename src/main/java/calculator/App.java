cc0a2b4e58d65c0c6581c00ed7623a1d6ab70154
Robbie <robbie331@>
2024-04-29 20:58:36 +0900

    level1 4

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -6,35 +6,42 @@ public class App {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("첫 번째 숫자를 입력하세요.");
-        int num1 = sc.nextInt();
-        System.out.println("두 번째 숫자를 입력하세요.");
-        int num2 = sc.nextInt();
+        boolean flag = true;
+        while (flag) {
+            System.out.println("첫 번째 숫자를 입력하세요.");
+            int num1 = sc.nextInt();
+            System.out.println("두 번째 숫자를 입력하세요.");
+            int num2 = sc.nextInt();
 
-        System.out.println("사칙연산 기호를 입력하세요.");
-        char operator = sc.next().charAt(0);
-
-        int result = 0;
-        switch (operator) {
-            case '+' :
-                result = num1 + num2;
-                break;
-            case '-' :
-                result = num1 - num2;
-                break;
-            case '*' :
-                result = num1 * num2;
-                break;
-            case '/' :
-                if (num2 == 0) {
-                System.out.println("분모에 0을 입력하셨습니다.");
-                } else {
+            System.out.println("사칙연산 기호를 입력하세요.");
+            char operator = sc.next().charAt(0);
+            int result = 0;
+            switch (operator) {
+                case '+':
+                    result = num1 + num2;
+                    break;
+                case '-':
+                    result = num1 - num2;
+                    break;
+                case '*':
                     result = num1 * num2;
-                }
-                break;
-            default:
-                System.out.println("연산자를 잘 못 입력하셨습니다.");
+                    break;
+                case '/':
+                    if (num2 == 0) {
+                        System.out.println("분모에 0을 입력하셨습니다.");
+                    } else {
+                        result = num1 / num2;
+                    }
+                    break;
+                default:
+                    System.out.println("연산자를 잘 못 입력하셨습니다.");
+            }
+            System.out.println("결과: " + result);
+            System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
+            String exit = sc.next();
+            if (exit.equals("exit")) {
+                flag = false;
+            }
         }
-        System.out.println("결과: " + result);
     }
 }
