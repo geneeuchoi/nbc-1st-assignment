37708c12154dbcb3272d484466c6fdbce67ddfc8
Robbie <robbie331@>
2024-04-29 20:50:19 +0900

    level1 3

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -13,5 +13,28 @@ public class App {
 
         System.out.println("사칙연산 기호를 입력하세요.");
         char operator = sc.next().charAt(0);
+
+        int result = 0;
+        switch (operator) {
+            case '+' :
+                result = num1 + num2;
+                break;
+            case '-' :
+                result = num1 - num2;
+                break;
+            case '*' :
+                result = num1 * num2;
+                break;
+            case '/' :
+                if (num2 == 0) {
+                System.out.println("분모에 0을 입력하셨습니다.");
+                } else {
+                    result = num1 * num2;
+                }
+                break;
+            default:
+                System.out.println("연산자를 잘 못 입력하셨습니다.");
+        }
+        System.out.println("결과: " + result);
     }
 }
